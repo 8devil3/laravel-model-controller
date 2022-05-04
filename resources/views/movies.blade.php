555e6f6060208eee8b53e44d061f6042f49925ad
8devil3 <8dev.il3@gmail.com>
2022-05-04 15:38:34 +0200

    aggiunto voto in stelline

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', 'Movies')
 
+
 @section('mainContent')
 <main>
    @foreach ($movies as $movie)
@@ -10,8 +11,22 @@
       <h3>{{ $movie['original_title'] }}</h3>
       <p>{{ $movie['nationality'] }}</p>
       <p>{{ $movie['date'] }}</p>
-      <p>{{ $movie['vote'] }}</p>
+      <div>
+         @php
+            $fullStars = intval(round($movie['vote']/2));
+            $emptyStars = 5 - intval(round($movie['vote']/2));
+         @endphp
+         @for ($i = 0; $i < $fullStars; $i++)
+            <i class="fa-solid fa-star"></i>
+         @endfor
+         @for ($i = 0; $i < $emptyStars; $i++)
+         <i class="fa-regular fa-star"></i>
+      @endfor
+      </div>
    </article>
    @endforeach
 </main>
 @endsection
+
+
+
