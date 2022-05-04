b95083513db818f0cb77a7ce0ee64f3f103e5d79
8devil3 <8dev.il3@gmail.com>
2022-05-04 16:37:41 +0200

    completato stile

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -8,21 +8,24 @@
    <div class="container">
       @foreach ($movies as $movie)
       <article>
-         <h2>Titolo: <em>{{ $movie['title'] }}</em></h2>
-         <h3>Titolo originale: <em>{{ $movie['original_title'] }}</em></h3>
-         <p>Nazionalità: <em>{{ $movie['nationality'] }}</em></p>
-         <p>Data: {{ date_format(date_create_from_format('Y-m-d', $movie['date']), 'd/m/Y') }}</p>
-         <div>Rating:
-            @php
-               $fullStars = intval(round($movie['vote']/2));
-               $emptyStars = 5 - $fullStars;
-            @endphp
-            @for ($i = 0; $i < $fullStars; $i++)
-               <i class="fa-solid fa-star"></i>
-            @endfor
-            @for ($i = 0; $i < $emptyStars; $i++)
-            <i class="fa-regular fa-star"></i>
-         @endfor
+         <h2>{{ $movie['title'] }}</h2>
+         <p>Titolo originale: <span class="desc">{{ $movie['original_title'] }}</span></p>
+         <p>Nazionalità: <span class="desc">{{ $movie['nationality'] }}</span></p>
+         <p>Data: <span class="desc">{{ date_format(date_create_from_format('Y-m-d', $movie['date']), 'd/m/Y') }}</span></p>
+         <div class="rating">
+            <p>Rating:</p>
+            <div>
+               @php
+                  $fullStars = intval(round($movie['vote']/2));
+                  $emptyStars = 5 - $fullStars;
+               @endphp
+               @for ($i = 0; $i < $fullStars; $i++)
+                  <i class="fa-solid fa-star"></i>
+               @endfor
+               @for ($i = 0; $i < $emptyStars; $i++)
+               <i class="fa-regular fa-star"></i>
+               @endfor
+            </div>
          </div>
       </article>
       @endforeach
