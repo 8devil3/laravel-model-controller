6d401eb32af97ed2785c85fbb911ebd489092af9
8devil3 <8dev.il3@gmail.com>
2022-05-05 10:17:49 +0200

    corretto controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,6 +10,6 @@ class PageController extends Controller
     public function index(){
         $movies = Movies::orderBy('title')->get(); //ritorna una collection, non un array semplice
 
-        return view('movies', compact('movies')); //con la funzione compact() trasformo la collection in un array e lo nomino 'movies'.
+        return view('movies', compact('movies'));
     }
 }
